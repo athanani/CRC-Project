9eb2276af93b2029f55d575bace2fb94919fe8fd
Thanos Nakas <athanani@csd.auth.gr>
2022-08-24 11:47:46 +0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/CRC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CRC.dir/main.cpp.obj"
+  "CRC.exe"
+  "CRC.exe.manifest"
+  "CRC.pdb"
+  "libCRC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
